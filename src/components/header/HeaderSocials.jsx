c51c99cace97095f576f78d81fe4de61a7b20854
Meet116 <56951062+Meet116@users.noreply.github.com>
2023-05-05 11:50:55 -0400

    Update HeaderSocials.jsx

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,7 +5,7 @@ import {FaGithub} from 'react-icons/fa'
 const HeaderSocials = () => {
   return (
   <div className="header__socials">
-      < a href="https://www.linkedin.com/in/meet-luhar-420017273" target="_blank"><BsLinkedin /></a>
+      < a href="https://www.linkedin.com/in/meet-luhar" target="_blank"><BsLinkedin /></a>
       < a href="https://github.com/Meet116" target="_blank"><FaGithub /></a>
   </div>
     )
